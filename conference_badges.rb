a4f8f6cfaab4840813249d59b03a415b074a514d
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-02 02:50:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(attendees)
   return "Hello, my name is #{attendees}."
 end
 
-def batch_badge_creator(attendees)
+def batch_badge_creator(badges)
   attendees.each do |badges|
   end
 end
